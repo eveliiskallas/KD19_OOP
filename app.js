b43695a4d3a646f91df5c3edcbef71edacb50a03
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-11 11:43:11 +0200

    Lisatud veel Window objekte

--- app.js
+++ app.js
@@ -1,17 +1,63 @@
 // Window objects / methods / properties
 
-// alert
+// ALERT
 
 // window.alert('Tere Hommikust!')
 // alert('Tere Hommikust!')
 
-// prompt
-const sisend = prompt('Sisesta oma nimi');
-alert('Tere Hommikust, ' + sisend + '!');
+// PROMT
 
-//confirm
-if(confirm ('Kas oled nõus?')){
-    console.log('Jah');
-} else {
-    console.log('Ei');
-};
+// const sisend = prompt('Sisesta oma nimi');
+// alert('Tere Hommikust, ' + sisend + '!');
+
+//CONFIRM
+
+// if(confirm ('Kas oled nõus?')){
+//     console.log('Jah');
+// } else {
+//     console.log('Ei');
+// }
+
+// VÄLIMÕÕDUD
+
+let valiKõrgus = window.outerHeight;
+let valiLaius = window.outerWidth;
+
+// console.log(valiKõrgus);
+// console.log(valiLaius);
+
+// SISEMÕÕDUD
+
+let siseKõrgus = window.innerHeight;
+let siseLaius = window.innerWidth;
+
+// console.log(siseKõrgus);
+// console.log(siseLaius);
+
+// SCROLL 
+
+// scrolY = window.scrollY;
+// scrolX = window.scrollX;
+// console.log(scrolY);
+// console.log(scrolX);
+
+// LOCATION 
+
+let objekt = window.location;
+let hostname = window.location.hostname;
+let port = window.location.port;
+let href = window.location.href;
+let search = window.location.search;
+console.log(objekt);
+console.log(hostname);
+console.log(port);
+console.log(href);
+console.log(search);
+
+// REDIRECT 
+
+// window.location.href = 'https://khk.ee';
+
+// RELOAD 
+
+// window.location.reload();
