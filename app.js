1678edf4cdd0a478d2b2a0e481f403c8ca219392
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-11 11:23:36 +0200

    Lisatud Window meetodid

---
+++ app.js
@@ -0,0 +1,17 @@
+// Window objects / methods / properties
+
+// alert
+
+// window.alert('Tere Hommikust!')
+// alert('Tere Hommikust!')
+
+// prompt
+const sisend = prompt('Sisesta oma nimi');
+alert('Tere Hommikust, ' + sisend + '!');
+
+//confirm
+if(confirm ('Kas oled nõus?')){
+    console.log('Jah');
+} else {
+    console.log('Ei');
+};
