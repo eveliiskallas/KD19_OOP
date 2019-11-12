e6eaf3705a0917a7ff1e0ea8219dab661e8c47d9
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-12 15:05:25 +0200

    Dokument ja tema omadused

--- app.js
+++ app.js
@@ -1,63 +1,18 @@
-// Window objects / methods / properties
+// DOCUMENT 
 
-// ALERT
+let sisu;
 
-// window.alert('Tere Hommikust!')
-// alert('Tere Hommikust!')
+sisu = document;  // sisu html'is
+sisu = document.all; // html tagid kasutamisjärjekorras
+sisu = document.all[2]; // konkreetne html tag
+sisu = document.all.length; // palju on html tagi kasutusel
+//DOKUMENDI OMADUSED
+sisu = document.head; // loeb ainult head'i siseseid andmeid 
+sisu = document.body; // loeb ainult body siseseid andmeid
+sisu = document.doctype; // millist HTML versipooni kasutatakse
+sisu = document.domain; // milline on domeen
+sisu = document.URL; // näidatakse täis aadressit
+sisu = document.characterSet; // millist meta andmestikku kasutatakse
+sisu = document.contentType; // millist content type'i kasutatakse
 
-// PROMT
-
-// const sisend = prompt('Sisesta oma nimi');
-// alert('Tere Hommikust, ' + sisend + '!');
-
-//CONFIRM
-
-// if(confirm ('Kas oled nõus?')){
-//     console.log('Jah');
-// } else {
-//     console.log('Ei');
-// }
-
-// VÄLIMÕÕDUD
-
-let valiKõrgus = window.outerHeight;
-let valiLaius = window.outerWidth;
-
-// console.log(valiKõrgus);
-// console.log(valiLaius);
-
-// SISEMÕÕDUD
-
-let siseKõrgus = window.innerHeight;
-let siseLaius = window.innerWidth;
-
-// console.log(siseKõrgus);
-// console.log(siseLaius);
-
-// SCROLL 
-
-// scrolY = window.scrollY;
-// scrolX = window.scrollX;
-// console.log(scrolY);
-// console.log(scrolX);
-
-// LOCATION 
-
-let objekt = window.location;
-let hostname = window.location.hostname;
-let port = window.location.port;
-let href = window.location.href;
-let search = window.location.search;
-console.log(objekt);
-console.log(hostname);
-console.log(port);
-console.log(href);
-console.log(search);
-
-// REDIRECT 
-
-// window.location.href = 'https://khk.ee';
-
-// RELOAD 
-
-// window.location.reload();
+console.log(sisu);
