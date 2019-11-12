dd1c2ffdff5d6668bb8d516a1f976aa921bed081
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-12 15:43:50 +0200

    Dokumendi omadused queryselectory abil

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 // DOCUMENT METHODS - DOKUMENDI MEETODID
 // SINGLE ELEMENT SELECTOR - ÜKSIKUTE ELEMENTIDE SELEKTOR
 
-sisu = document.getElementById('title'); // html sisu 
-
-id = document.getElementById('title').id; // id
-klass = document.getElementById('title').className; // klass
+sisu = document.querySelector('#title'); // id järgi 
+sisu = document.querySelector('.card-title'); // klassi järgi 
+sisu = document.querySelector('h5'); // elemendi järgi 
 
 // STIILI MUUTMINE
-// sisu.style.background = '#333'; // stiili lisamine
-document.getElementById('title').style.background = '#eee';
-sisu.style.color = '#FFF';
-sisu.style.padding = '5px';
-// sisu.style.display = 'none';
-
-// SISU MUUTMINE 
-sisu.textContent = 'Minu Ülesanne';
-sisu.innerText  = 'Just Minu Ülesanne';
-sisu.innerHTML = '<span style="color:green">Vabad Ülesanded</span>'
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue'; // Üksik selector
+document.querySelector('li:last-child').style.color = 'red'; // Üksik selector
 
 console.log(sisu);
 
