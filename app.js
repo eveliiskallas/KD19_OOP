d049c5a9ae9723ee70fb1bfa23fd68bddf2a385b
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-12 15:21:50 +0200

    Dokumendi piltide ja scriptide omadused

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ sisu = document;  // sisu html'is
 sisu = document.all; // html tagid kasutamisjärjekorras
 sisu = document.all[2]; // konkreetne html tag
 sisu = document.all.length; // palju on html tagi kasutusel
-//DOKUMENDI OMADUSED - OBJEKTI OMADUSED
+// DOKUMENDI OMADUSED - OBJEKTI OMADUSED
 sisu = document.head; // loeb ainult head'i siseseid andmeid 
 sisu = document.body; // loeb ainult body siseseid andmeid
 sisu = document.doctype; // millist HTML versipooni kasutatakse
@@ -28,6 +28,12 @@ sisu = document.links[0]; // esimene link
 sisu = document.links[0].className; // esimese klassi link
 sisu = document.links[0].classList[1]; // klasside massiiv ja sealt elemendi lugemine
 
+// PILDID
+sisu = document.images;
 
+//SCRIPTID
+sisu = document.scripts;
+sisu = document.scripts[0].getAttribute('src'); // materialized script 
+sisu = document.scripts[1].getAttribute('src'); // lokaalne script
 
 console.log(sisu);
