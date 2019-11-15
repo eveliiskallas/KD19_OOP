c4bbfbb5fa160ab16c88e07966b9d6a0f2decd18
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-15 13:45:21 +0200

    Node Omadused ja Meetodid (Parent, Children and sibling)

--- app.js
+++ app.js
@@ -1,45 +1,56 @@
 // DOCUMENT METHODS - DOKUMENDI MEETODID
-// MULTIPLY ELEMENT SELECTOR - MITME ELEMENTI SELEKTOR
-
-let sisu = document.getElementsByClassName('collection-item');
-// klassi nime järgi 
-console.log(sisu);
-console.log(sisu[0]);
-sisu[0].style.color = 'orange'
-sisu[2].textContent = 'Õpi Arvutivõrgud'
-
-// querySelector + klassi järgi
-let nimekirjaElemendid = document.querySelector('ul').getElementsByClassName('collection-item');
-
-// nimekirja nime järgi 
-nimekirjaElemendid = document.getElementsByTagName('li');
-
-// teisendame HTMLCollection massiiviks (array)
-nimekirjaElemendid = Array.from(nimekirjaElemendid);
-
-// massiivi meetodite kasutamine
-nimekirjaElemendid.reverse();
-
-// MASSIIVI LÄBIOMINE TSÜKLIS
-
-// for
-for(let i = 0; i < nimekirjaElemendid.length; i++){
-    // console.log(nimekirjaElemendid[i].className);
-    nimekirjaElemendid[i].style.background = '#ddd'
-    nimekirjaElemendid[1].style.background = '#f4f4f4'
-}
-// forEach
-// nimekirjaElemendid.forEach(function(element) {
-//     console.log(element);
-//     element.style.background = '#ddd';
-// });
-
-let nimekiriPaaritu = document.querySelectorAll('li:nth-child(odd)');
-console.log(nimekiriPaaritu);
-
-let nimekiriPaaris = document.querySelectorAll('li:nth-child(even)');
-console.log(nimekiriPaaris);
-
 // NODE OMADUSED JA MEETODID
 
-let nimekiri = document.querySelectorAll('ul');
+let nimekiri = document.querySelectorAll('ul.collection');
+let nimekirjaElement = document.querySelectorAll('li.collection-item')
+
+console.log(nimekiri);
+console.log(nimekirjaElement);
+
+nimekiri = document.querySelector('ul.collection');
+console.log(nimekiri);
+
+sisu = nimekiri.childNodes;
+sisu = nimekiri.childNodes[0];
+sisu = nimekiri.childNodes[0].nodeName;
+sisu = nimekiri.childNodes[5].nodeType;
+
+/* 
+erinevad node tüübid 
+1 - element 
+2 - attribute 
+3 - tekst 
+8 - kommentaar 
+9 - dokument 
+10 - doctype 
+*/
+
+sisu = nimekiri.children;
+sisu = nimekiri.children[1];
+nimekiri.children[1].textContent = 'Õpi Rohkem JavaScripti!'
+sisu = nimekiri.children[2].children[0].id = 'katse';
+console.log(sisu);
+console.log(nimekiri.children[2].children[0]);
+
+sisu = nimekiri.firstChild; // node type 
+sisu = nimekiri.firstElementChild; // value
+
+sisu = nimekiri.lastChild;  
+sisu = nimekiri.lastElementChild;  
+
+sisu = nimekiri.childElementCount; 
+
+nimekirjaElement = document.querySelector('li.collection-item:nth-child(2)'); 
+sisu = nimekirjaElement.parentNode;
+sisu = nimekirjaElement.parentElement;
+sisu = nimekirjaElement.parentElement.parentElement;
+
+sisu = nimekirjaElement.nextSibling; // node type
+sisu = nimekirjaElement.nextElementSibling; // value
+
+sisu = nimekirjaElement.previousSibling;  
+sisu = nimekirjaElement.previousElementSibling;  
+
+
+console.log(sisu);
+
