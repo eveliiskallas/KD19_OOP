782a0404a510e3673ca4a4016c46f7f0d0decead
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-20 09:53:40 +0200

    Sündmused - hiire sündmused

--- app.js
+++ app.js
@@ -2,25 +2,40 @@
 
 const kustutaKoik = document.querySelector('.clear-tasks');
 
-kustutaKoik.addEventListener('click', vajutaNupp);
+const pealkiri = document.querySelector('h5');
 
-function vajutaNupp(e){
-    let sisu = e;
-    // Target
-    sisu = e.target;
-    sisu = e.target.id;
-    sisu = e.target.className;
-    sisu = e.target.classList;
+const kaart = document.querySelector('.card');
 
-    // Tüüp 
-    sisu = e.type;
+// click
+// kustutaKoik.addEventListener('click', syndmus);
 
-    // Sündmuse koordinaadid browseri akna suhtes
-    sisu = e.clientY;
-    sisu = e.clientX;
+// double click
+// kustutaKoik.addEventListener('dblclick', syndmus);
 
-    // Sündmuse koordinaadid elemendi enda suhtes
-    sisu = e.offsetY;
-    sisu = e.offsetX;
-    console.log(sisu);
+// mouse down
+// kustutaKoik.addEventListener('mousedown', syndmus);
+
+// mouse up
+// kustutaKoik.addEventListener('mouseup', syndmus);
+
+// mouse enter
+// kaart.addEventListener('mouseenter', syndmus);
+
+// mouse leave
+// kaart.addEventListener('mouseleave', syndmus);
+
+// mouse over / out 
+// kaart.addEventListener('mouseover', syndmus);
+
+// mouse move
+// kaart.addEventListener('mousemove', syndmus);
+
+
+function syndmus(e){
+  // logi sündmuse tüüp 
+  console.log(`Sündmuse tüüp: ${e.type}`);
+  // muuda teksti sisu: HiirX: x koord Hiir Y: y koord
+  pealkiri.textContent = `HiirX: ${e.offsetX} HiirY: ${e.offsetY} `;
+  // koosta body värv rgb(Xkoord, Ykoord, 40);
+  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
 }
