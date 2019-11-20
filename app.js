bbd9374f8c636a1aca81b03e345dc39e6abac4e5
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-20 10:58:08 +0200

    Sündmused - klaviatuuri sündmused

--- app.js
+++ app.js
@@ -7,8 +7,39 @@ const pealkiri = document.querySelector('h5');
 // kustutame lisaUusYlesanne väärtus 
 lisaUusYlesanne.value = '';
 
+// klaviatuur 
+
+// keydown 
+// lisaUusYlesanne.addEventListener('keydown', syndmus);
+
+// keyup 
+// lisaUusYlesanne.addEventListener('keyup', syndmus);
+
+// keypress 
+// lisaUusYlesanne.addEventListener('keypress', syndmus);
+
+// focus  
+// lisaUusYlesanne.addEventListener('focus', syndmus);
+
+// blur  
+// lisaUusYlesanne.addEventListener('blur', syndmus);
+
+// paste  
+// lisaUusYlesanne.addEventListener('paste', syndmus);
+
+// cut  
+// lisaUusYlesanne.addEventListener('cut', syndmus);
+
+// input  
+// lisaUusYlesanne.addEventListener('input', syndmus);
+
+// input  
+// lisaUusYlesanne.addEventListener('input', syndmus);
+
+
+
 // vormi saatmine 
-form.addEventListener('submit', syndmus);
+// form.addEventListener('submit', syndmus);
 
 function syndmus(e){
   // logi sündmuse tüüp 
