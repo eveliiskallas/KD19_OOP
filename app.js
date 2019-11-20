26540e3f3a3f0423b4d928ed9114bc3bc4132a91
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-20 10:29:04 +0200

    Sündmused - klaviatuur ja vorm

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
 // SÜNDMUSED
 
-const kustutaKoik = document.querySelector('.clear-tasks');
-
+const form = document.querySelector('form');
+const lisaUusYlesanne = document.getElementById('task');
 const pealkiri = document.querySelector('h5');
 
-const kaart = document.querySelector('.card');
+// kustutame lisaUusYlesanne väärtus 
+lisaUusYlesanne.value = '';
 
-// click
-// kustutaKoik.addEventListener('click', syndmus);
-
-// double click
-// kustutaKoik.addEventListener('dblclick', syndmus);
-
-// mouse down
-// kustutaKoik.addEventListener('mousedown', syndmus);
-
-// mouse up
-// kustutaKoik.addEventListener('mouseup', syndmus);
-
-// mouse enter
-// kaart.addEventListener('mouseenter', syndmus);
-
-// mouse leave
-// kaart.addEventListener('mouseleave', syndmus);
-
-// mouse over / out 
-// kaart.addEventListener('mouseover', syndmus);
-
-// mouse move
-// kaart.addEventListener('mousemove', syndmus);
-
+// vormi saatmine 
+form.addEventListener('submit', syndmus);
 
 function syndmus(e){
   // logi sündmuse tüüp 
   console.log(`Sündmuse tüüp: ${e.type}`);
-  // muuda teksti sisu: HiirX: x koord Hiir Y: y koord
-  pealkiri.textContent = `HiirX: ${e.offsetX} HiirY: ${e.offsetY} `;
-  // koosta body värv rgb(Xkoord, Ykoord, 40);
-  document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 40)`;
+
+  // kontrollime vormi kaudu saadetud väärtust 
+  console.log(lisaUusYlesanne.value);
+
+  // väljastame ülesanne HTML-is 
+  pealkiri.innerText = lisaUusYlesanne.value;
+
 }
