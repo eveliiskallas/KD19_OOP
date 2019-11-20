60578e223e63e38fa444be9fc8b3fa1798bad531
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-20 09:24:45 +0200

    Sündmused - Sündmusobjekti omadused

--- app.js
+++ app.js
@@ -2,7 +2,25 @@
 
 const kustutaKoik = document.querySelector('.clear-tasks');
 
-kustutaKoik.addEventListener('click', function(e){
-    console.log('Vajutatud');
-    // e.preventDefault();
-});
+kustutaKoik.addEventListener('click', vajutaNupp);
+
+function vajutaNupp(e){
+    let sisu = e;
+    // Target
+    sisu = e.target;
+    sisu = e.target.id;
+    sisu = e.target.className;
+    sisu = e.target.classList;
+
+    // Tüüp 
+    sisu = e.type;
+
+    // Sündmuse koordinaadid browseri akna suhtes
+    sisu = e.clientY;
+    sisu = e.clientX;
+
+    // Sündmuse koordinaadid elemendi enda suhtes
+    sisu = e.offsetY;
+    sisu = e.offsetX;
+    console.log(sisu);
+}
