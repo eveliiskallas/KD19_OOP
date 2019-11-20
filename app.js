651dd3d2637e540775a473ace5b8e4c2a2ed9189
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-20 08:54:55 +0200

    Sündmused - Funktsioon sündmuse sees

--- app.js
+++ app.js
@@ -1,38 +1,8 @@
-// DOCUMENT METHODS - DOKUMENDI MEETODID
-// ELEMENTIDE KUSTUTAMINE 
+// SÜNDMUSED
 
-// Leian antud DOM'is kõik Listi elemendid
-const liElemendid = document.querySelectorAll('li');
+const kustutaKoik = document.querySelector('.clear-tasks');
 
-// Kustuta konkreetne element 
-liElemendid[2].remove();
-
-// Leia antud DOM'is ul element 
-const ulElement = document.querySelector('ul');
-
-// kustuta antud elemendist li 
-//li on ul lapsed 
-ulElement.removeChild(liElemendid[1]);
-
-// KLASSI JA ATRIBUUDI KUSTUTAMINE
-
-// nimekirja element
-const liElement = liElemendid[0]; 
-// kustutamislink
-const link = liElement.children[0];
-
-// Klass
-let sisu = link.className;
-sisu = link.classList;
-sisu = link.classList[1]; //kustutas kindla klassi nime 
-link.classList.remove('secondary-content');
-sisu = link;
-
-// Atribuut 
-sisu = link.getAttribute('href');
-link.setAttribute('href', 'https://google.com')
-link.setAttribute('title', 'Google')
-link.removeAttribute('title');
-sisu = link;
-
-console.log(sisu);
+kustutaKoik.addEventListener('click', function(e){
+    console.log('Vajutatud');
+    // e.preventDefault();
+});
