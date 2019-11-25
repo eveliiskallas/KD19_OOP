ea2564f76e77db0d35bd872511b8eed3b9fe2a62
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-25 15:15:40 +0200

    Raamatud - raamatu lisamise funktsioon

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function Raamat(p, a, i){
 }
 
 // Kasutajaliides - KL 
-
+//konstruktor 
 function KL(){
 
 }
@@ -18,6 +18,21 @@ KL.prototype.puhastaSisend = function(){
     document.getElementById('title').value = '';
     document.getElementById('author').value = '';
     document.getElementById('isbn').value = '';
+}
+
+// raamatu lisamine tabelisse 
+KL.prototype.lisaRaamatTabelisse = function(r){
+    // loome tabeli rea 
+    const rida = document.createElement('tr');
+    // täidame rea tabeli andmetega 
+    rida.innerHTML = `
+        <td>${r.pealkiri}</td>
+        <td>${r.autor}</td>
+        <td>${r.isbn}</td>
+    `
+    // lisame rea tabeli sisse 
+    tabel = document.getElementById('book-list');
+    tabel.appendChild(rida);
 }
 
 // Kirjeldame raamatu lisamise sündmuse 
@@ -38,6 +53,9 @@ function lisaRaamat(e){
 
     // loome kasutajaliidese objekti temaga opereerimiseks
     const kl = new KL();
+
+    // lisame sisestatud raamatu tabelisse 
+    kl.lisaRaamatTabelisse(raamat);
 
     // puhastame väljad sisestatud andmetest
     kl.puhastaSisend();
