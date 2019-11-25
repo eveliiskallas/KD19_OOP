dccc92a778844e07370c904b29fb92ed7e24aa94
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-25 11:08:13 +0200

    Vanuse Arvutamne

--- app.js
+++ app.js
@@ -1,44 +1,20 @@
-// STORAGE 
+// INIMESE KIRJELDUS
+let eesnimi, perenimi; 
+let synnikuupaev; 
 
-// SALVESTAME VÄÄRTUSE 
-// localStorage.setItem('eesnimi', 'Eveliis');
-// localStorage.setItem('perenimi', 'Kallas');
-
-// EEMALDAME VÄÄRTUSE
-// localStorage.removeItem('nimi');
-
-// LOEME VÄÄRTUST 
-// const  eesnimi = localStorage.getItem('eesnimi');
-// console.log(eesnimi);
-// const  perenimi = localStorage.getItem('perenimi');
-// console.log(perenimi);
-
-// PUHASTAME STORAGE-i 
-// localStorage.clear();
-
-document.querySelector('form').addEventListener('submit', salvesta);
-
-function salvesta(e){
-    const UusYlesanne = document.getElementById('task').value;
-    // localStorage.setItem('tasks', UusYlesanne);
-    let ylesanded;
-    if(localStorage.getItem('tasks') == null){
-        ylesanded = [];
-        console.log(ylesanded)
-    } else {
-        // console.log('olemas, tuleb lugeda');
-        ylesanded = JSON.parse(localStorage.getItem('tasks'));
-    }
-    console.log(ylesanded);
-    ylesanded.push(UusYlesanne); // lisame element massiivi lõppu 
-    localStorage.setItem('tasks', JSON.stringify(ylesanded)); // lisame andmed LS sisse 
-    e.preventDefault();
+function taisNimi(eesnimi, perenimi){
+    return `${eesnimi} ${perenimi}`;
 }
 
-// loeme sisestatud ülesanded
-const ylesanded = JSON.parse(localStorage.getItem('tasks'));
-ylesanded.forEach(ylesanne => {
-    console.log(ylesanne);
-});
+function arvutaVanus(synnikuupaev){
+    synnikuupaev = new Date(synnikuupaev);
+    vaheSekundites = Date.now() - synnikuupaev.getTime();
+    vanusDate = new Date(vaheSekundites);
+    aastaDate = vanusDate.getUTCFullYear();
+    vanus = aastaDate - 1970;
 
+    return `Vanus: ${vanus}`;
+}
 
+console.log(taisNimi("Eveliis", "Kallas"));
+console.log(arvutaVanus("1998-06-03"));
