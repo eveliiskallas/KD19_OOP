0b80e1d88ce2d3c6fb35243a4b24d7c3bdc9154d
eveliiskallas <eveliis.kallas@khk.ee>
2019-11-25 11:32:58 +0200

    Inimese Kirjeldus OOP abil - lisatud ees ja perenimi

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 // INIMESE KIRJELDUS OOP ABIL 
 // inimese konstruktor
 
-function Isik(n, skp){ //konstruktor 
-    this.nimi = n;
+function Isik(e, p, skp){ //konstruktor - tegeleb antud objekti loomisega 
+    this.eesnimi = e;
+    this.perenimi = p;
     this.synnikuupaev = new Date(skp);
+
+    // väljasta täisnimi
+    this.taisNimi = function(){
+        return `${this.eesnimi} ${this.perenimi}`;
+    }
 
     // arvuta vanus 
     this.arvutaVanus = function(){
@@ -15,7 +21,7 @@ function Isik(n, skp){ //konstruktor
     }
 
 }
-const eveliis = new Isik('Eveliis', '03-06-1998');
-const nele = new Isik('Nele', '11-21-2008');
-console.log(eveliis.arvutaVanus());
-console.log(nele.arvutaVanus());
+const eveliis = new Isik('Eveliis', 'Kallas', '03-06-1998');
+const nele = new Isik('Nele', 'Kallas',  '11-21-2008');
+console.log(eveliis.taisNimi());
+console.log(nele.taisNimi());
