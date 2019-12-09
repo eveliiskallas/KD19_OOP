c58d9a164c61d9abde9ba123ac55fc8e08fc6d65
eveliiskallas <eveliis.kallas@khk.ee>
2019-12-09 14:48:43 +0200

    Weather API kasutamine

---
+++ Weather/api.js
@@ -0,0 +1,54 @@
+function weatherBalloon( cityID ) {
+    var key = '730be19993d9d96dbb348d1229bbd78e';
+    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+        console.log(data);
+    })
+    .catch(function() {
+        // catch any errors
+    });
+}
+
+    window.onload = function() {
+    weatherBalloon( 7522434 );
+}
+
+function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	document.getElementById('description').innerHTML = d.weather[0].description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+}
+
+function weatherBalloon( cityID ) {
+	var key = '730be19993d9d96dbb348d1229bbd78e';
+	fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
+	.then(function(resp) { return resp.json() }) // Convert data to json
+	.then(function(data) {
+		drawWeather(data); // Call drawWeather
+	})
+	.catch(function() {
+		// catch any errors
+	});
+}
+
+function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	var description = d.weather[0].description;
+	
+	document.getElementById('description').innerHTML = description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+	
+	if( description.indexOf('rain') > 0 ) {
+  	document.body.className = 'rainy';
+  } else if( description.indexOf('cloud') > 0 ) {
+  	document.body.className = 'cloudy';
+  } else if( description.indexOf('sunny') > 0 ) {
+  	document.body.className = 'sunny';
+  }
+}
