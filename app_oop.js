65628cd6d0a9e814d6e5115ae0b5afbfc75e921a
eveliiskallas <eveliis.kallas@khk.ee>
2019-12-09 13:06:04 +0200

    Raamatu kustutamine KL sündmus

--- app_oop.js
+++ app_oop.js
@@ -62,7 +62,7 @@ class KL {
     // kustutaRaamatTabelist 
     kustutaRaamatTabelist(kustutaElement){
         if(kustutaElement.className === 'kustuta'){
-            tabeliRida = kustutaElement.parentElement.parentElement;
+            const tabeliRida = kustutaElement.parentElement.parentElement;
             tabeliRida.remove();
             return true;
         }
@@ -73,8 +73,7 @@ class KL {
 document.getElementById('book-form').addEventListener('submit', lisaRaamat);
 
 // Raamatu lisamise funktsioon   
-function lisaRaamat(e){
-            
+function lisaRaamat(e){        
     const pealkiri = document.getElementById('title').value;
     const autor = document.getElementById('author').value;
     const isbn = document.getElementById('isbn').value;
@@ -92,17 +91,37 @@ function lisaRaamat(e){
         // muidu lisame sisestatud raamatu tabelisse 
         // lisame sisestatud raamatu tabelisse 
         kl.lisaRaamatTabelisse(raamat);
-
         // loome LS objekti funktsiooni kutsumiseks 
         // const ls = new LS();
-
         // lisame raamatu andmed  local storage'sse
         // ls.salvestaRaamat(raamat);
-
         kl.teade('Raamat on lisatud tabelisse', 'valid');    
     };
-
     // puhastame väljad sisestatud andmetest
     kl.puhastaSisend();
 }
-e.preventDefault();
+
+// Raamatu kustutamise sündmus 
+document.getElementById('book-list').addEventListener('click', kustutaRaamat);
+function kustutaRaamat(e){
+    // loome kasutajaliidese objekti temaga opereerimiseks
+    const kl = new KL();
+
+    // kutsutakse tabelist oleva raamatu kustutamise funktsiooni 
+    // loome X link, millele clickime, et kustutada 
+    const X = e.target; 
+    // saame kustutava raamatu isbn kätte 
+    isbn = X.parentElement.previousElementSibling.textContent;
+    console.log(isbn);
+    // kustutame andmed tabeli väljundist 
+    kl.kustutaRaamatTabelist(X);
+    // kustutame andmed local storagest 
+    // const ls = new LS()
+    // onKustutatud = ls.kustutaRaamatLS(isbn);
+    // väljastame vastava teate 
+    // if(onKustutatud){
+    kl.teade('Raamat on kustutatud', 'valid');
+    // }
+    e.preventDefault();
+}
+
