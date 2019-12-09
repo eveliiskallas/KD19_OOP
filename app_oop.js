2bd1f39554d2ef8b2636abb46d5a8743c93fc740
eveliiskallas <eveliis.kallas@khk.ee>
2019-12-09 12:33:12 +0200

    Raamatu klass on olemas

--- app_oop.js
+++ app_oop.js
@@ -1,2 +1,13 @@
 // app_oop.js
 
+// Raamat
+
+class Raamat {
+    constructor (a, p, i){
+        this.autor = a;
+        this.pealkiri = p;
+        this.isbn = i;
+    }
+}
+const raamat = new Raamat ('Tammsaare', 'Tõde ja Õigus', 1234); 
+console.log(raamat);
